c35a1f6afc8ca0a4ab96133c1d1911f120d2599a
tarikwaleed <takizee53@gmail.com>
2023-03-12 08:13:54 +0200

    added admin-dashboard component and enable routing

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+
+
+
+@NgModule({
+  declarations: [
+    AdminDashboardComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AdminModule { }
--- src/app/navigation/navigation-routing.module.ts
+++ src/app/navigation/navigation-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { BooksListComponent } from '../book/components/books-list/books-list.component';
 import { AuthorListComponent } from '../author/components/author-list/author-list.component';
 import { CategoryListComponent } from '../category/components/category-list/category-list.component';
+import { AdminDashboardComponent } from '../admin/components/admin-dashboard/admin-dashboard.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/books', pathMatch: 'full' },
   { path: 'books', component: BooksListComponent },
   { path: 'authors', component: AuthorListComponent },
   { path: 'categories', component: CategoryListComponent },
+  { path: 'admin', component: AdminDashboardComponent },
 
 ];
 
