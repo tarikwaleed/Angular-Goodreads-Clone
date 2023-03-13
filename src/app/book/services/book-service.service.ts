ff0bf2eaa33eb8550fb6c8c1bd6117e907e44d49
tarikwaleed <takizee53@gmail.com>
2023-03-13 05:35:50 +0200

    added book-service file

--- src/app/book/services/book-service.service.ts
+++ src/app/book/services/book-service.service.ts
@@ -1,39 +1,34 @@
 
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {  Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class BookServiceService {
-private url=""
-  constructor(private _HttpClient:HttpClient) { }
+  private url = ""
+  constructor(private _HttpClient: HttpClient) { }
 
 
-  getAllBook():Observable<any>
-  {
+  getAllBook(): Observable<any> {
     return this._HttpClient.get(this.url)
   }
 
-  getBookById(id:string):Observable<any>
-   {
-     return this._HttpClient.get(this.url)
-   }
+  getBookById(id: string): Observable<any> {
+    return this._HttpClient.get(this.url)
+  }
 
-    editBook(id:string):Observable<any>
-   {
-     return this._HttpClient.post(this.url,id)
-   }
+  editBook(id: string): Observable<any> {
+    return this._HttpClient.post(this.url, id)
+  }
 
-   deleteBook(id:string):Observable<any>
-  {
-     return this._HttpClient.delete(this.url)
-   }
-  
-   AddToFavorite(id:string):Observable<any>
-   {
-     return this._HttpClient.post(this.url,id)
-   }
+  deleteBook(id: string): Observable<any> {
+    return this._HttpClient.delete(this.url)
+  }
+
+  AddToFavorite(id: string): Observable<any> {
+    return this._HttpClient.post(this.url, id)
+  }
 }
 
 
