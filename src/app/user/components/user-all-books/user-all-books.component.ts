9f386c06a3b4a35514de8de54630ec4e4b9c241f
tarikwaleed <takizee53@gmail.com>
2023-03-15 22:23:58 +0200

    some edits on user-all-books class and html

--- src/app/user/components/user-all-books/user-all-books.component.ts
+++ src/app/user/components/user-all-books/user-all-books.component.ts
@@ -28,42 +28,13 @@ export class UserAllBooksComponent {
     this.productService.getProducts().then(data => this.products = data);
   }
 
-  openNew() {
-    this.product = {};
-    this.submitted = false;
-    this.productDialog = true;
-  }
 
-  deleteSelectedProducts() {
-    this.confirmationService.confirm({
-      message: 'Are you sure you want to delete the selected products?',
-      header: 'Confirm',
-      icon: 'pi pi-exclamation-triangle',
-      accept: () => {
-        this.products = this.products.filter(val => !this.selectedProducts.includes(val));
-        this.selectedProducts = []
-        this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Products Deleted', life: 3000 });
-      }
-    });
-  }
 
   editProduct(product: Product) {
     this.product = { ...product };
     this.productDialog = true;
   }
 
-  deleteProduct(product: Product) {
-    this.confirmationService.confirm({
-      message: 'Are you sure you want to delete ' + product.name + '?',
-      header: 'Confirm',
-      icon: 'pi pi-exclamation-triangle',
-      accept: () => {
-        this.products = this.products.filter(val => val.id !== product.id);
-        this.product = {};
-        this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Product Deleted', life: 3000 });
-      }
-    });
-  }
   applyFilterGlobal($event: any, stringVal: any) {
     this.dt!.filterGlobal(($event.target as HTMLInputElement).value, stringVal);
   }
