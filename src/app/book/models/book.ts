6a057ea23142c25d330c077f7a069daa104b4217
tarikwaleed <takizee53@gmail.com>
2023-03-16 23:11:54 +0200

    category name rendered successfully

--- src/app/book/models/book.ts
+++ src/app/book/models/book.ts
@@ -1,12 +1,12 @@
+import { Category } from "src/app/category/models/category"
 import { Author } from "../../author/models/author"
-import { Genre } from "./genre"
 
 export interface Book {
     id:number
     title:string
     isbn:string
     author:Author
-    genre:Genre
+    category:Category
     summary:string
     imageUrl:string
 }
