d4df472b886673810e55ae60b2ea8c35d713642b
norhan-khaled99 <61085153+norhan-khaled99@users.noreply.github.com>
2023-03-21 23:14:31 +0200

    add some feateures to landing page ❤

--- src/app/navigation/navigation-routing.module.ts
+++ src/app/navigation/navigation-routing.module.ts
@@ -9,7 +9,7 @@ import { AuthGuard } from '../admin/guards/auth/auth.guard';
 import { NotAuthErrorComponent } from '../admin/components/not-auth-error/not-auth-error.component';
 import { BookDetailsComponent } from '../book/components/book-details/book-details.component';
 import { UserProfileComponent } from '../user/components/user-profile/user-profile.component';
-
+import { LandingPageComponent } from '../landing-page/landing-page.component';
 const routes: Routes = [
   { path: '', redirectTo: '/books', pathMatch: 'full' },
   { path: 'books', component: BooksListComponent },
@@ -19,6 +19,7 @@ const routes: Routes = [
   { path: 'admin', component: AdminDashboardComponent, canActivate: [AuthGuard] },
   { path: 'not-auth-error', component: NotAuthErrorComponent },
   { path: 'profile', component: UserProfileComponent },
+ {path:'/home',redirectTo:'',component:LandingPageComponent}
 
 ];
 
