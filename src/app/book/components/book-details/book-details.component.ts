13b20173ec8da3aaf5a0e78311aba208c0067e6f
tarikwaleed <takizee53@gmail.com>
2023-03-23 06:53:50 +0200

    bookData rendered successfully in bookDetails

--- src/app/book/components/book-details/book-details.component.ts
+++ src/app/book/components/book-details/book-details.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UserBookService } from 'src/app/user/services/user-book.service';
-import { BookDataService } from '../../services/book-data.service';
+import { BookDetailsService } from '../../services/book-details.service';
 
 @Component({
   selector: 'app-book-details',
@@ -9,26 +9,17 @@ import { BookDataService } from '../../services/book-data.service';
   styleUrls: ['./book-details.component.css']
 })
 export class BookDetailsComponent {
-  constructor(private route: ActivatedRoute, private userBookService: UserBookService, private bookDataService: BookDataService) { }
+  constructor(private route: ActivatedRoute, private userBookService: UserBookService, private bookDetailsService: BookDetailsService) { }
   bookId!: string
   bookData!: any
   userBookData!: any
-  coverImage!: string
 
   ngOnInit() {
     this.bookId = this.route.snapshot.params['id'];
-    this.bookDataService.getBook(this.bookId).subscribe(data => {
+    this.bookDetailsService.getBook(this.bookId).subscribe(data => {
       this.bookData = data
-      console.log(this.bookData);
-      const originalCoverImage = this.bookData.coverImage.split('/')
-      if (originalCoverImage[0] === 'uploads') {
-        this.coverImage = originalCoverImage[1]
-      }
-      else {
-        this.coverImage = originalCoverImage[0]
-      }
-    })
 
+    })
   }
 
 }
--- src/app/book/services/book-details.service.ts
+++ src/app/book/services/book-details.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BookDataService } from './book-data.service';
 import { Observable, map } from 'rxjs';
-
+import { Rating } from "../models/rating";
 @Injectable({
   providedIn: 'root'
 })
@@ -9,8 +9,39 @@ export class BookDetailsService {
 
   constructor(private bookDataService: BookDataService) { }
   getBook(bookId: string): Observable<any> {
-    return this.bookDataService.getBook(bookId).subscribe(data=)
 
+    return this.bookDataService.getBook(bookId).pipe(
+      map((book: any) => {
+        //sanitize the data
+        const author_name: string = `${book.author[0].first_name} ${book.author[0].last_name}}`
+        let coverImage!: string
+        const originalCoverImage = book.coverImage.split('/')
+        if (originalCoverImage[0] === 'uploads') {
+          coverImage = originalCoverImage[1]
+        }
+        else {
+          coverImage = originalCoverImage[0]
+        }
+        let totoalRating: number=0
+        book.ratings.map((rating: Rating) => {
+          totoalRating += rating.rating
+        })
+        console.log(totoalRating);
+        const averageRating=totoalRating/book.ratingCount
+        // then return
+        return {
+          _id: book._id,
+          author_name: author_name,
+          title: book.title,
+          coverImage: coverImage,
+          summary: book.summary,
+          ratingCount: book.ratingCount,
+          averageRating:averageRating
+
+        };
+
+      })
+    );
 
   }
 }
