308e771dea22ca6e3a9c4cd6f187cb9ccaf36668
tarikwaleed <takizee53@gmail.com>
2023-03-24 06:32:50 +0200

    block admin dashboard if user is not admin

--- src/app/admin/guards/auth/auth.guard.ts
+++ src/app/admin/guards/auth/auth.guard.ts
@@ -1,19 +1,26 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/registration/services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard {
-  loggedin: Boolean = true
+  isAdmin: Boolean = false
 
   // in the constructor inject authservice and toaster  service and router 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private authService: AuthService) {
+    this.authService.getUser().subscribe(data => {
+      if (data?.role === 'admin') {
+        this.isAdmin = true
+      }
+    })
+  }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    if (this.loggedin) {
+    if (this.isAdmin) {
       return true;
     } else {
       this.router.navigate(['/not-auth-error']);
